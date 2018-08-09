a1ae541f721ed2f351cf137e7f94416681bb1cab
Евгений Войтенко <e.voitenko@mangotele.com>
2018-08-09 14:09:47 +0300

    Сделал формат дат

--- app/src/main/java/com/example/evv/mtsfarm/utils/ExcelParser.java
+++ app/src/main/java/com/example/evv/mtsfarm/utils/ExcelParser.java
@@ -6,8 +6,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -22,37 +26,39 @@ public class ExcelParser {
         //InputStream ExcelFileToRead = new FileInputStream(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Data.xlsx"));
 
         InputStream inputStream = new FileInputStream(new File("/sdcard/Download/Data.xlsx"));
-        //XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
-
         XSSFWorkbook wb = new XSSFWorkbook(inputStream);
 
-        XSSFWorkbook test = new XSSFWorkbook();
+        for (int pageCount = 0; pageCount < wb.getNumberOfSheets(); pageCount++) {
+            XSSFSheet sheet = wb.getSheetAt(pageCount);
+            XSSFRow row;
+            XSSFCell cell;
 
-        XSSFSheet sheet = wb.getSheetAt(0);
-        XSSFRow row;
-        XSSFCell cell;
+            Iterator rows = sheet.rowIterator();
 
-        Iterator rows = sheet.rowIterator();
+            while (rows.hasNext()) {
+                row = (XSSFRow) rows.next();
+                Iterator cells = row.cellIterator();
+                while (cells.hasNext()) {
+                    cell = (XSSFCell) cells.next();
 
-        while (rows.hasNext()) {
-            row = (XSSFRow) rows.next();
-            Iterator cells = row.cellIterator();
-            while (cells.hasNext()) {
-                cell = (XSSFCell) cells.next();
+                    if (cell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(cell.getStringCellValue() + " ");
+                    } else if (cell.getCellTypeEnum() == CellType.NUMERIC) {
+                        if (pageCount > 0) {
+                            Date javaDate = DateUtil.getJavaDate((double) cell.getNumericCellValue());
+                            System.out.println(new SimpleDateFormat("yyyy-dd-MM").format(javaDate));
+                        } else {
+                            System.out.print(cell.getNumericCellValue() + " ");
+                        }
 
-                if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
-                    System.out.print(cell.getStringCellValue() + " ");
-                } else if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
-                    System.out.print(cell.getNumericCellValue() + " ");
-                } else {
-                    //U Can Handel Boolean, Formula, Errors
+                    } else {
+                        //U Can Handel Boolean, Formula, Errors
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
-
     }
-
 }
 
 
