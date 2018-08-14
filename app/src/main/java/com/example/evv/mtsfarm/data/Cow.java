7d9bfd7c53a05c7fd7d48b4d42d694e608ea1f53
Евгений Войтенко <e.voitenko@mangotele.com>
2018-08-14 16:09:08 +0300

    Сделал парсинг коров

--- app/src/main/java/com/example/evv/mtsfarm/data/Cow.java
+++ app/src/main/java/com/example/evv/mtsfarm/data/Cow.java
@@ -1,11 +1,11 @@
 package com.example.evv.mtsfarm.data;
 
 public class Cow {
-    private int id;
-    private String name;
-    private int age;
-    private int weight;
-    private String status;
-    private int herd;
-    private int farm;
+    public int id;
+    public String name;
+    public int age;
+    public int weight;
+    public String status;
+    public int herd;
+    public String farm;
 }
--- app/src/main/java/com/example/evv/mtsfarm/utils/ExcelParser.java
+++ app/src/main/java/com/example/evv/mtsfarm/utils/ExcelParser.java
@@ -1,9 +1,12 @@
 package com.example.evv.mtsfarm.utils;
 
 import com.example.evv.mtsfarm.App;
+import com.example.evv.mtsfarm.data.Cow;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jxl.Cell;
 import jxl.CellType;
@@ -16,30 +19,45 @@ public class ExcelParser {
         Workbook w;
         try {
             w = Workbook.getWorkbook(file);
-            // Get the first sheet
-            for (int page = 0; page <= w.getNumberOfSheets(); page++) {
-                Sheet sheet = w.getSheet(page);
-                // Loop over first 10 column and lines
-
-                for (int j = 0; j < sheet.getColumns(); j++) {
-                    for (int i = 0; i < sheet.getRows(); i++) {
-                        Cell cell = sheet.getCell(j, i);
-                        CellType type = cell.getType();
-                        if (type == CellType.LABEL) {
-                            System.out.println("I got a label "
-                                    + cell.getContents());
-                        }
-
-                        if (type == CellType.NUMBER) {
-                            System.out.println("I got a number "
-                                    + cell.getContents());
-                        }
-
-                    }
-                }
-            }
+            Sheet sheet = w.getSheet(0);
+            parseCows(sheet);
         } catch (BiffException e) {
             e.printStackTrace();
         }
+    }
+
+    private List<Cow> parseCows(Sheet sheet) {
+        List<Cow> result = new ArrayList<>();
+        for (int row = 1; row < sheet.getRows(); row++) {
+            Cow cow = new Cow();
+            for (int col = 0; col < sheet.getColumns(); col++) {
+                Cell cell = sheet.getCell(col, row);
+                switch (col) {
+                    case 0:
+                        cow.id = Integer.valueOf(cell.getContents());
+                        break;
+                    case 1:
+                        cow.name = cell.getContents();
+                        break;
+                    case 2:
+                        cow.age = Integer.valueOf(cell.getContents());
+                        break;
+                    case 3:
+                        cow.weight = Integer.valueOf(cell.getContents());
+                        break;
+                    case 4:
+                        cow.status = cell.getContents();
+                        break;
+                    case 5:
+                        cow.herd = Integer.valueOf(cell.getContents());
+                        break;
+                    case 6:
+                        cow.farm = cell.getContents();
+                        break;
+                }
+                result.add(cow);
+            }
+        }
+        return result;
     }
 }
