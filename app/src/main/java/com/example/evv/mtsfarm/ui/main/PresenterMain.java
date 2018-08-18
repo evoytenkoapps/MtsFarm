4a9cfedbb8ea77d7e960358b771bd5f30a1e1d8e
Евгений Войтенко <e.voitenko@mangotele.com>
2018-08-18 13:22:15 +0300

    Сделал стате

--- app/src/main/java/com/example/evv/mtsfarm/ui/main/PresenterMain.java
+++ app/src/main/java/com/example/evv/mtsfarm/ui/main/PresenterMain.java
@@ -9,7 +9,9 @@ import com.example.evv.mtsfarm.repo.FarmRepository;
 import com.example.evv.mtsfarm.ui.AppBasePresenter;
 import com.example.evv.mtsfarm.ui.BaseView;
 
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -17,9 +19,13 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class PresenterMain extends AppBasePresenter implements ContractMain.Presenter {
+
     private final String TAG = this.getClass().getSimpleName();
     private FarmRepository mRepo = App.getRepo();
-
+    private ArrayDeque<String> mQue = new ArrayDeque<>();
+    private final String SHOW_LOADING = "SHOW_LOADING";
+    private final String HIDE_LOADING = "HIDE_LOADING";
+    private final String REFRESH_DATA = "REFRESH_DATA";
 
     @Override
     public void getData() {
@@ -60,12 +66,12 @@ public class PresenterMain extends AppBasePresenter implements ContractMain.Pres
 
                 @Override
                 public void showLoading() {
-
+                    mQue.addFirst(SHOW_LOADING);
                 }
 
                 @Override
                 public void hideLoading() {
-
+                    mQue.addFirst(HIDE_LOADING);
                 }
 
                 @Override
@@ -80,12 +86,29 @@ public class PresenterMain extends AppBasePresenter implements ContractMain.Pres
 
                 @Override
                 public void refreshData(List<Cow> data) {
-
+                    mQue.addFirst(REFRESH_DATA);
                 }
             };
         }
         return view;
     }
 
-
+    @Override
+    public void attachView(BaseView view) {
+        super.attachView(view);
+        String state;
+        while ((state = mQue.pollLast()) != null) {
+            switch (state) {
+                case SHOW_LOADING:
+                    ((ContractMain.View) view).showLoading();
+                    break;
+                case HIDE_LOADING:
+                    ((ContractMain.View) view).hideLoading();
+                    break;
+                case REFRESH_DATA:
+                    getData();
+                    break;
+            }
+        }
+    }
 }
